05b169fadf98c8dca72d7a3777481a681185efd5
Are Strom <are.strom@gmail.com>
2019-07-09 18:04:10 -0700

    Initial commit

---
+++ survey_comment_content.R
@@ -0,0 +1,26 @@
+#=========================================================
+# HTML content definition of header accordian
+#========================================================
+
+# Define the survey data content
+survey_comment_content = tags$div(
+  actionButton(inputId = "comment_add", label = "New", class = "new_button"),
+  actionButton(inputId = "comment_edit", label = "Edit", class = "edit_button"),
+  actionButton(inputId = "comment_delete", label = "Delete", class = "delete_button"),
+  br(),
+  br(),
+  uiOutput("area_surveyed_select", inline = TRUE),
+  uiOutput("abundance_condition_select", inline = TRUE),
+  uiOutput("count_condition_select", inline = TRUE),
+  uiOutput("survey_timing_select", inline = TRUE),
+  uiOutput("stream_condition_select", inline = TRUE),
+  uiOutput("weather_type_select", inline = TRUE),
+  uiOutput("visibility_condition_select", inline = TRUE),
+  uiOutput("stream_flow_select", inline = TRUE),
+  uiOutput("visibility_type_select", inline = TRUE),
+  textInput(inputId = "submitter_input", label = "submitter", value = NA, width = "100px"),
+  br(),
+  br(),
+  br(),
+  DT::DTOutput("survey_comment")
+)
